c324e02c5b41b807fc86aaf4bee42dde529131b3
a1994lex <alexandrapowley@gmail.com>
2018-02-09 18:24:30 -0700

    Fix the Command Serialization by doing Double Reflecting

---
+++ shared/src/main/java/com/groupryan/shared/Serializer.java
@@ -0,0 +1,11 @@
+package com.groupryan.shared;
+
+/**
+ * ${CLASS} in com.groupryan.shared
+ *
+ * @author Alex POwley
+ * @version 1.0 2/9/2018.
+ */
+
+public class Serializer {
+}
---
+++ shared/src/main/java/com/groupryan/shared/models/Serializable.java
@@ -0,0 +1,11 @@
+package com.groupryan.shared.models;
+
+/**
+ * ${CLASS} in com.groupryan.shared.models
+ *
+ * @author Alex POwley
+ * @version 1.0 2/9/2018.
+ */
+
+public interface Serializable {
+}
