83906b230605d249c71f0dae9890b1094bc7ab6e
darthzeran <ryan.mortensen60@gmail.com>
2018-02-09 21:34:49 -0700

    fixed server

--- client/src/test/java/com/groupryan/client/ClientCommunicatorTest.java
+++ client/src/test/java/com/groupryan/client/ClientCommunicatorTest.java
@@ -2,6 +2,7 @@ package com.groupryan.client;
 
 import com.groupryan.shared.commands.ServerCommand;
 import com.groupryan.shared.commands.ServerCommandFactory;
+import com.groupryan.shared.models.Game;
 import com.groupryan.shared.models.User;
 import com.groupryan.shared.results.CommandResult;
 
@@ -18,13 +19,16 @@ public class ClientCommunicatorTest extends TestCase {
 
         ServerCommand sc=scf.createRegisterCommand(user);
         CommandResult cr=(CommandResult) cc.sendCommand("Register", sc);
-        int i=0;
+
 
         sc =scf.createLoginCommand(user);
          cr= (CommandResult) cc.sendCommand("Login", sc);
 
         sc =scf.createLoginCommand(user);
         cr= (CommandResult) cc.sendCommand("Login", sc);
+        sc=scf.createCreateCommand(new Game("YOLO", "123", 3));
+        cr=(CommandResult) cc.sendCommand("Create", sc);
+
     int j=0;
     }
 
