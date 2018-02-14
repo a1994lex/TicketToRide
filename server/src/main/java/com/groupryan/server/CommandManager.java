396386fd04251ec942356898cf5e26173294048d
bengu3 <benjee.012@gmai.com>
2018-02-13 17:33:16 -0700

    CommandManager stuff Polller done

--- server/src/main/java/com/groupryan/server/CommandManager.java
+++ server/src/main/java/com/groupryan/server/CommandManager.java
@@ -1,12 +1,16 @@
 package com.groupryan.server;
 
+import com.groupryan.server.models.RootServerModel;
 import com.groupryan.shared.commands.ClientCommand;
 import com.groupryan.shared.commands.ClientCommandFactory;
 import com.groupryan.shared.models.Color;
 import com.groupryan.shared.models.Game;
 import com.groupryan.shared.models.User;
+import com.groupryan.shared.results.CommandResult;
+import com.groupryan.shared.utils;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -16,11 +20,14 @@ import java.util.Map;
 
 public class CommandManager {
 
-    List<ClientCommand> commands=new ArrayList<>();
+    Map<User, List<ClientCommand>> usersCommands = new HashMap<>();
     private static CommandManager instance;
 
     public static CommandManager getInstance() {
         if (instance == null) {
+
+            // TODO make usersCommands Map
+
             instance = new CommandManager();
         }
         return instance;
@@ -33,38 +40,76 @@ public class CommandManager {
         this.factory = new ClientCommandFactory();
     }
 
-    public List<ClientCommand> getCommands(int index) {
-        return commands.subList(index, commands.size());
+    public List<ClientCommand> getCommands(User user) {
+        List<ClientCommand> commands = this.usersCommands.get(user);
+        this.usersCommands.put(user, new ArrayList<>());
+        return commands;
+    }
+
+    private void _addCommandToMap(ClientCommand command, Object o, String commandType) {
+        for (Map.Entry<User, List<ClientCommand>> entry : this.usersCommands.entrySet()) {
+            switch (commandType) {
+                case utils.JOIN_GAME:
+                    User u = (User) o;
+                    if (!entry.getKey().equals(u)) {
+                        List<ClientCommand> commands = entry.getValue();
+                        commands.add(command);
+                        this.usersCommands.put(u, commands);
+                    }
+                    break;
+                case utils.CREATE_GAME:
+                    Game g = (Game) o;
+                    String userId = (String) new ArrayList(g.getUsers().keySet()).get(0);
+                    User user = RootServerModel.getUser(userId);
+                    if (!entry.getKey().equals(user)) {
+                        List<ClientCommand> commands = entry.getValue();
+                        commands.add(command);
+                        this.usersCommands.put(user, commands);
+                    }
+                    break;
+                case utils.START_GAME:
+                    Game g2 = (Game) o;
+                    String userId2 = (String) new ArrayList(g2.getUsers().keySet()).get(0);
+                    User user2 = RootServerModel.getUser(userId2);
+                    if (!entry.getKey().equals(user2)) {
+                        List<ClientCommand> commands = entry.getValue();
+                        commands.add(command);
+                        this.usersCommands.put(user2, commands);
+                    }
+                    break;
+            }
+        }
     }
 
     public ClientCommand makeJoinGameCommand(Game game, User user, String userColor) {
         ClientCommand command = factory.createJoinGameCommand(game, user, userColor);
-        commands.add(command);
+        this._addCommandToMap(command, user, utils.JOIN_GAME);
         return command;
     }
 
     public ClientCommand makeCreateGameCommand(Game game) {
         ClientCommand command = factory.createCreateGameCommand(game);
-         commands.add(command);
-         return command;
+        this._addCommandToMap(command, game, utils.CREATE_GAME);
+        return command;
     }
 
     public ClientCommand makeStartGameCommand(Game game) {
         ClientCommand command = factory.createStartGameCommand(game);
-         commands.add(command);
+        this._addCommandToMap(command, game, utils.START_GAME);
         return command;
     }
 
     public ClientCommand makeLoginCommand(User user) {
         ClientCommand command = factory.createLoginCommand(user);
-         //commands.add(command);
+        this.usersCommands.put(user, new ArrayList<>());
         return command;
     }
 
     public ClientCommand makeRegisterCommand(User user) {
         ClientCommand command = factory.createRegisterCommand(user);
-        // commands.add(command);
+        this.usersCommands.put(user, new ArrayList<>());
         return command;
 
     }
+
 }
--- server/src/main/java/com/groupryan/server/facades/GetCommandsFacade.java
+++ server/src/main/java/com/groupryan/server/facades/GetCommandsFacade.java
@@ -1,6 +1,7 @@
 package com.groupryan.server.facades;
 
 import com.groupryan.server.CommandManager;
+import com.groupryan.shared.models.User;
 import com.groupryan.shared.results.CommandResult;
 import com.groupryan.shared.utils;
 
@@ -9,18 +10,11 @@ import com.groupryan.shared.utils;
  */
 
 public class GetCommandsFacade {
-    private int index;
 
-    public CommandResult getCommandList(){
-
-        //calls the command manager with the index to get the command list
-        CommandResult cr=new CommandResult();
-        cr.setClientCommands(CommandManager.getInstance().getCommands(index));
-        //converts it to a Commandresult
+    public CommandResult getCommandList(User user) {
+        CommandResult cr = new CommandResult();
+        cr.setClientCommands(CommandManager.getInstance().getCommands(user));
         cr.setResultType(utils.VALID);
         return cr;
     }
-
-
-
 }
