459a894c20fa88021002873e1855abe65b797e64
bengu3 <benjee.012@gmai.com>
2018-02-28 19:12:07 -0700

    Poller and Start Game working

--- clientapp/src/main/java/async/GamePoller.java
+++ clientapp/src/main/java/async/GamePoller.java
@@ -12,19 +12,32 @@ import java.util.TimerTask;
 public class GamePoller {
     private String gameId;
     private String playerId = RootClientModel.getUser().getUsername();
-    public GamePoller(String gameId) {
+    private Timer timer = new Timer(true);
+    private static GamePoller instance;
+
+    private GamePoller(String gameId) {
         this.gameId = gameId;
     }
 
+    public static GamePoller get(String gameId) {
+        if (instance == null) {
+            instance = new GamePoller(gameId);
+        }
+        return instance;
+    }
+
     public void poll() {
-        Timer timer = new Timer(true);
-        timer.scheduleAtFixedRate(new TimerTask() {
+        this.timer.scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
                 GamePollerTask gamePollerTask = new GamePollerTask(gameId, playerId);
                 gamePollerTask.execute();
             }
         }, 0, 500);
+    }
+
+    public void stop() {
+        this.timer.cancel();
     }
 
 }
--- clientapp/src/main/java/com/example/clientapp/LobbyActivity.java
+++ clientapp/src/main/java/com/example/clientapp/LobbyActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -59,7 +60,6 @@ public class LobbyActivity extends AppCompatActivity implements ILobbyView {
         mStartGameBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 if (mGame.getMaxPlayers() == (double) mGame.getUsers().size()) {
                     LobbyPresenter.startGame(mGame);
                 }
@@ -77,7 +77,7 @@ public class LobbyActivity extends AppCompatActivity implements ILobbyView {
     public void onStartGame() {
         Intent i = new Intent(LobbyActivity.this, GamePlayActivity.class);
         startActivity(i);
-        mAdapter.notifyDataSetChanged();
+//        mAdapter.notifyDataSetChanged();
     }
 
     @Override
