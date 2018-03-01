13a1ac73bfedbf62d087b33fc4e4b6cd4a691a23
a1994lex <alexandrapowley@gmail.com>
2018-02-28 21:20:26 -0700

    Create mapToObject for Player Class

--- shared/src/main/java/com/groupryan/shared/models/Player.java
+++ shared/src/main/java/com/groupryan/shared/models/Player.java
@@ -1,7 +1,10 @@
 package com.groupryan.shared.models;
 
+import com.google.gson.internal.LinkedTreeMap;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by arctu on 2/17/2018.
@@ -12,47 +15,72 @@ public class Player {
     List<Route> routes;
     int points;
     List<DestCard> destCards;
-    List<Card> trainCards;
+    List<TrainCard> trainCards;
     int trainPieces;
     String username;
 
-    public Player(String color, List<DestCard> destCards, List<Card> trainCards, String username){
-        this.color=color;
-        this.routes=new ArrayList<>();
-        this.points=0;
-        this.destCards=destCards;
-        this.trainCards=trainCards;
-        this.trainPieces=45;
-        this.username=username;
+    public static Player mapToObject(LinkedTreeMap map) {
+        String username = (String) map.get("username");
+        String color = (String) map.get("color");
+        int points = (Integer) map.get("points");
+        // TODO: fix the route map to object
+        int trainpieces = (Integer) map.get("trainPieces");
+        List<DestCard> destCards = new ArrayList<>();
+        ArrayList<LinkedTreeMap> dcards = (ArrayList<LinkedTreeMap>) map.get("destCards");
+        for (LinkedTreeMap cardMap : dcards) {
+            destCards.add(DestCard.mapToObject(cardMap));
+        }
+        List<TrainCard> trainCards = new ArrayList<>();
+        ArrayList<LinkedTreeMap> tCards = (ArrayList<LinkedTreeMap>) map.get("trainCards");
+        for (LinkedTreeMap tMap : tCards) {
+            trainCards.add(TrainCard.mapToObject(tMap));
+        }
+        return new Player(color, destCards, trainCards, username);
+
+
     }
 
-    public Player(String username, String color){ // for testing only
+    public Player(String color, List<DestCard> destCards, List<TrainCard> trainCards, String username) {
+        this.color = color;
+        this.routes = new ArrayList<>();
+        this.points = 0;
+        this.destCards = destCards;
+        this.trainCards = trainCards;
+        this.trainPieces = 45;
+        this.username = username;
+    }
+
+    public Player(String username, String color) { // for testing only
         this.username = username;
         this.color = color;
     }
 
-    public String getUsername(){
+    public String getUsername() {
         return username;
     }
-    public void removeDestinationCard(Card c){
+
+    public void removeDestinationCard(Card c) {
         destCards.remove(c);
     }
-    public void removeTrainCard(Card c){
-        destCards.remove(c);}
 
-    public Stat makeStat(){
+    public void removeTrainCard(Card c) {
+        destCards.remove(c);
+    }
+
+    public Stat makeStat() {
         return new Stat(username, points, trainPieces, trainCards.size(), destCards.size());
     }
 
-    public void addDestCards(List<DestCard> cards){
-        for (DestCard c:cards) {
+    public void addDestCards(List<DestCard> cards) {
+        for (DestCard c : cards) {
             destCards.add(c);
         }
     }
 
-    public void addTrainCards(List<TrainCard> cards){
-        for (Card c:cards) {
-        trainCards.add(c);
-    }}
+    public void addTrainCards(List<TrainCard> cards) {
+        for (TrainCard c : cards) {
+            trainCards.add(c);
+        }
+    }
 
 }
