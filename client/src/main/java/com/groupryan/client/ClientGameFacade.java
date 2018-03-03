949576fce292ae9f842c3b170aa5a8edff53125f
darthzeran <ryan.mortensen60@gmail.com>
2018-03-02 18:41:32 -0700

    stat testing

--- client/src/main/java/com/groupryan/client/ClientGameFacade.java
+++ client/src/main/java/com/groupryan/client/ClientGameFacade.java
@@ -1,6 +1,7 @@
 package com.groupryan.client;
 
 import com.groupryan.client.models.RootClientModel;
+import com.groupryan.shared.models.Stat;
 
 /**
  * Created by clairescout on 2/27/18.
@@ -14,7 +15,9 @@ public class ClientGameFacade {
         RootClientModel.getInstance().getCurrentGame().updateHistory(event);
     }
 
-    public void updateStat(){}
+    public void updateStat(Stat stat){
+        RootClientModel.getInstance().getCurrentGame().updateStat(stat);
+    }
 
     public void updateChat(String msg){ RootClientModel.getCurrentGame().updateChat(msg);}
 }
