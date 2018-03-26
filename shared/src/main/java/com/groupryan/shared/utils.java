93fc20af12e5c27276d64489e8525d4b505c0934
Alex Powley <alexandrapowley@gmail.com>
2018-03-26 13:08:16 -0600

    sash

--- shared/src/main/java/com/groupryan/shared/utils.java
+++ shared/src/main/java/com/groupryan/shared/utils.java
@@ -41,7 +41,7 @@ public class utils {
     public static final String EXEC_COMMAND = "/executeCommand";  // url for command API
     public static final String SERVER_HOST = "localhost";
     public static final int PORT_NUMBER = 8080;
-    public static String URL_PREFIX = "http://" + "10.24.67.78" + ":" + PORT_NUMBER;
+    public static String URL_PREFIX = "http://" + "10.24.64.212" + ":" + PORT_NUMBER;
 
     public static final String HTTP_POST = "POST";
     public static final String COMMAND_NAME = "CommandName";   // HTTP request header to determine
@@ -93,6 +93,6 @@ public class utils {
     }
 
     public static void setUrlPrefix(String host){
-        URL_PREFIX = "http://" + host + ":" + PORT_NUMBER;
+//        URL_PREFIX = "http://" + host + ":" + PORT_NUMBER;
     }
 }
