7ba7c3dd9dcca44b93d7df7cf0cbc1355aabb010
Claire Gammon <claire.gammon@gmail.com>
2018-03-29 10:52:52 -0600

    fixed calculate destinations

--- server/src/main/java/com/groupryan/server/facades/EndGameFacade.java
+++ server/src/main/java/com/groupryan/server/facades/EndGameFacade.java
@@ -21,8 +21,9 @@ import java.util.List;
 
 public class EndGameFacade {
 
-    HashMap<String, EndGameStat> usernameToStat = new HashMap<>();
-    String winner;
+    private HashMap<String, EndGameStat> usernameToStat = new HashMap<>();
+    private String winner;
+    private boolean finished = false;
 
     public EndGameFacade(){}
 
@@ -108,8 +109,10 @@ public class EndGameFacade {
                 }
 
                 //dfs return true if they have finished the route and increases their points, false if they haven't and decreases points
-                if(dfs(startCity, dfsPrep(player.getRoutes()), startRoute, destinationCard.getCityTwo(), player.getRoutes() )){
+                dfs(startCity, dfsPrep(player.getRoutes()), startRoute, destinationCard.getCityTwo(), player.getRoutes() );
+                if(finished){
                     usernameToStat.get(player.getUsername()).increaseReachedDestPoints(destinationCard.getValue());
+                    finished = false;
                 }
                 else{
                     usernameToStat.get(player.getUsername()).increaseUnreachedDestNegativePoints(destinationCard.getValue());
@@ -130,25 +133,24 @@ public class EndGameFacade {
         return idToVisited;
     }
 
-    private boolean dfs(String city, HashMap<Integer, Boolean> idToVisited, Route route, String finalDestination, List<Route> routes){
+    private void dfs(String city, HashMap<Integer, Boolean> idToVisited, Route route, String finalDestination, List<Route> routes){
         //depth first search to find if destination card is complete
 
         idToVisited.put(route.getId(), true);
         for (Route r: routes){
             if(r.getCityOne().equals(city) || r.getCityTwo().equals(city)){
                 if(r.getCityOne().equals(finalDestination) || r.getCityTwo().equals(finalDestination)){
-                    return true;
+                    finished = true;
                 }
                 if(!idToVisited.get(r.getId())){
                     String city2;
                     if(r.getCityOne().equals(city)){ city2 = r.getCityTwo(); }
                     else { city2 = r.getCityOne();}
-                    return dfs(city2, idToVisited, r, finalDestination, routes);
+                    dfs(city2, idToVisited, r, finalDestination, routes);
                 }
             }
         }
 
-        return false;
     }
 
     private Route getStart(String city, List<Route> routes){
