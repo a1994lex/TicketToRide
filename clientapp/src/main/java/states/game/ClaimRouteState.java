e6ef06969309f52a0bef39fe18cc1375d4aa98ce
Daniel Najarro <daniel.alonzo.najarro@gmail.com>
2018-03-30 16:29:25 -0600

    submit function in claim route state

--- clientapp/src/main/java/states/game/ClaimRouteState.java
+++ clientapp/src/main/java/states/game/ClaimRouteState.java
@@ -19,9 +19,8 @@ public class ClaimRouteState implements GameState {
 
     @Override
     public void submit(GamePlayPresenter gpp) {
-        gpp.callClaimRouteAsyncTask();
         gpp.setState(new InactiveState());
-        gpp.endTurn();
+        gpp.callClaimRouteAsyncTask();
     }
 
     @Override
