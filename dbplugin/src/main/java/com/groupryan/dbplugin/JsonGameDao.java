bba5f44d97d1a105efe0d9e95ffaa93eb1f54996
Daniel Najarro <daniel.alonzo.najarro@gmail.com>
2018-04-18 16:28:31 -0600

    pulling

--- dbplugin/src/main/java/com/groupryan/dbplugin/JsonGameDao.java
+++ dbplugin/src/main/java/com/groupryan/dbplugin/JsonGameDao.java
@@ -5,6 +5,8 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.stream.JsonReader;
 
 import java.util.ArrayList;
 import java.util.HashMap;
